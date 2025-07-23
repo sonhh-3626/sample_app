f721c472ca9c5d080cb19354e28b8086e6013012
hasonsk <sonhh2003@gmail.com>
2025-07-23 15:48:21 +0700

    Chapter 11

---
+++ app/controllers/account_activations_controller.rb
@@ -0,0 +1,34 @@
+class AccountActivationsController < ApplicationController
+  before_action :get_user, only: :edit
+
+  # GET /account_activations/:id/edit
+  def edit
+    if activate_for(@user)
+      flash[:success] = t "users.success.activated"
+      redirect_to @user
+    else
+      flash[:danger] = t "users.error.invalid_activation_link"
+      redirect_to root_url, status: :see_other
+    end
+  end
+
+  private
+
+  def get_user
+    @user = User.find_by email: params[:email]
+    return if @user
+
+    flash[:danger] = t "users.error.user_not_found"
+    redirect_to root_url, status: :see_other
+  end
+
+  def activate_for user
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.activate
+      log_in user
+      true
+    else
+      false
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   PERMITTED_ATTRIBUTES = %w(name email password password_confirmation).freeze
   before_save{self.email = email.downcase}
@@ -25,7 +28,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  scope :order_by_name, ->{order(name: :asc)}
+  scope :activated, ->{where(activated: true)}
+  scope :order_by_name, ->{activated.order(name: :asc)}
 
   class << self
     def digest string
@@ -52,13 +56,33 @@ class User < ApplicationRecord
     remember_digest || remember
   end
 
-  def authenticated? remember_token
-    return false if remember_digest.nil?
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
 
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update_attribute :remember_digest, nil
+  end
+
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  def downcase_email
+    email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
